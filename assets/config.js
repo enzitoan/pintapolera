a4d2650f5823b318fd22e8bb787273b7394dee07
Enzo Ahumada <enzo.ahumada@arkanosoft.com>
2022-04-27 12:21:54 -0400

    first commit

---
+++ assets/config.js
@@ -0,0 +1,39 @@
+// cpanel - site_templates/countdown_tech/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "",
+        logo: "Pinta Polera",
+        social: [
+            
+            
+            
+            {
+                icon: 'instagram',
+                link: "https:\/\/www.instagram.com\/pinta_polera"
+            },
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: '2020-05-01T04:00:00.000Z',
+            content: "",
+        }
+    ]
+};
